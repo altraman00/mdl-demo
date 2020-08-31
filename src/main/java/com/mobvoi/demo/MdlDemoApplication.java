bc31744a1fadc8981108d2cb35fb0a888c65d474
kun.xie <kun.xie@mobvoi.com>
2020-08-31 14:08:23 +0800

    springboot demo

---
+++ src/main/java/com/mobvoi/demo/MdlDemoApplication.java
@@ -0,0 +1,14 @@
+package com.mobvoi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdlDemoApplication.class, args);
+	}
+
+}
+
