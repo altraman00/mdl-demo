5664adcdf9cb8c839ffe20887c80c56e4ad33c62
kun.xie <kun.xie@mobvoi.com>
2020-10-20 14:53:17 +0800

    nil

---
+++ day02/aa.java
@@ -0,0 +1,11 @@
+/**
+ * @Project : mdl-demo
+ * @Package Name : PACKAGE_NAME
+ * @Description : TODO
+ * @Author : xiekun
+ * @Create Date : 2020年10月20日 14:52
+ * ----------------- ----------------- -----------------
+ */
+public class aa {
+
+}
