43da979cafc76ef9898d8fbdbac30fedaadb3bb4
kun.xie <kun.xie@mobvoi.com>
2021-01-02 22:50:08 +0800

    [tools-home]nil

--- tools-home/src/main/java/com/mdl/home/controller/UserToolsCategorySiteController.java
+++ tools-home/src/main/java/com/mdl/home/controller/UserToolsCategorySiteController.java
@@ -9,19 +9,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/home")
 @Controller
 public class UserToolsCategorySiteController {
 
-    @Autowired
-    private UserToolsCategorySiteService siteService;
+  @Autowired
+  private UserToolsCategorySiteService siteService;
 
-    @RequestMapping("/site")
-    public String home(Model model,String userId) {
-        List<UserToolsCategorySiteEntity> siteList = siteService.findByUserId(userId);
-        model.addAttribute("siteList", siteList);
-        return "home";
-    }
+  @RequestMapping("/site")
+  public String home(Model model
+      , @RequestParam(value = "userId", required = false, defaultValue = "1") String userId) {
+    List<UserToolsCategorySiteEntity> siteList = siteService.findByUserId(userId);
+    model.addAttribute("siteList", siteList);
+    return "tools_site";
+  }
 
 }
