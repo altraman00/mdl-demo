4025ee5ca69f23d88ff4c509a2ca9650c5650c6d
kun.xie <kun.xie@mobvoi.com>
2021-03-10 11:32:20 +0800

    [day01]添加责任链的另一种方式

--- day01/src/main/java/com/mdl/demo/patterns/chainpattern05/ExecuteHandler.java
+++ day01/src/main/java/com/mdl/demo/patterns/chainpattern05/ExecuteHandler.java
@@ -26,6 +26,33 @@ public class ExecuteHandler {
 
   private CostHandler handler;
 
+
+  /**
+   * 方法一：
+   * 在@PostConstruct标注的方法中将多个责任连接成链，然后返回第一个责任，在调用第一个责任内可根据逻辑决定是否执行下一个责任
+   *
+   * 方法二：
+   * 在@PostConstruct标注的方法中将多个责任直接放进有序list，然后在另一个方法中通过循环返回每一个责任的处理结果，
+   * 然后判断当前责任的结果是true还是false，决定循环是否执行下一个责任
+   * 例：
+   * public class ChainApplication {
+   *     public static void main(String[] args) {
+   *         HandlerChain handlerChain = new HandlerChain();
+   *         handlerChain.addHandler(Lists.newArrayList(new HandlerA(), new HandlerB()));
+   *         boolean resultFlag = handlerChain.handle();
+   *         if (!resultFlag) {
+   *             System.out.println("责任链中处理器不满足条件");
+   *         }
+   *     }
+   * }
+   *
+   * 作者：源码兴趣圈
+   * 链接：https://juejin.cn/post/6932058640677355534
+   * 来源：掘金
+   * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   *
+   * @return
+   */
   public CostHandler getHandler() {
     return handler;
   }
